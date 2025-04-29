3183049ec822524b64948b0f90e22581f12d81e1
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2025-04-29 13:14:40 +0330

    add cols to genre migration

--- database/migrations/2025_04_29_094244_create_genres_table.php
+++ database/migrations/2025_04_29_094244_create_genres_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
